5c57ed1984b4589e7baf13b76b54702bb2015222
MartaGnarta <martikajover@icloud.com>
2023-03-10 11:58:37 +0100

    TTF lib

--- Game.h
+++ Game.h
@@ -3,6 +3,7 @@
 #include "SDL/include/SDL.h"
 #include "SDL_image/include/SDL_image.h"
 #pragma comment(lib, "SDL_image/libx86/SDL2_image.lib")
+#pragma comment(lib, "SDL_TTF/libx86/SDL2_ttf.lib")
 #pragma comment( lib, "SDL/libx86/SDL2.lib" )
 #pragma comment( lib, "SDL/libx86/SDL2main.lib" )
 
